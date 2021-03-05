dcc39fe4e8d9e2990634bf803d2d000f7e965434
gigicupp <gigicupp@gmail.com>
2021-03-04 20:53:21 -0800

    mongoose.connect to hardcoded localhost

--- server/database/schema.js
+++ server/database/schema.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect(process.env.MONGO_URI, {
+  mongoose.connect('mongodb://localhost/:5007', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
