0b85e13cdbe27a1f6c34eb787a27440c0a451a61
gigicupp <gigicupp@gmail.com>
2021-03-13 14:07:44 -0800

    seeded user1 with 10M records.

--- server/database-couch/seed.js
+++ server/database-couch/seed.js
@@ -1,66 +1,55 @@
 require('dotenv').config();
 const faker = require('faker');
-const { AWS, S3 } = require('./aws.js');
+const { getImgKeys } = require('./aws.js');
 const nano = require('nano');
 const { createCouch } = require('./schema.js');
 
 const languages = ['English', 'Spanish', 'French', 'Portuguese', 'German', 'Italian', 'Cambodian', 'Thai', 'Shyriiwook'];
 
-let params = { Bucket: 'userservicebucket' };
-let listAllKeys = S3.listObjectsV2(params).promise();
+const seed = async (start, number) => {
+  const user1 = await createCouch();
+  const S3Url = await getImgKeys();
 
-const seedManyUsers = async (start, number) => {
-  const user = await createCouch();
-  const ids = [];
-  // batchSize determines how many users get created simultaneously
-  // Use batchSize to not overload image service with too many requests at once
-  // Batches of 100+ requests known to cause socket error
-  let batchSize = 100;
+  const seedManyUsers = async (start, number) => {
 
-  while (number > 0 && batchSize > 0) {
-    ids.push(start);
-    start++;
-    number--;
-    batchSize--;
-  }
-  return new Promise(async (resolve, reject) => {
-    await listAllKeys
-      .then(({ Contents }) => {
-        let allKeys = Contents.map(({ Key }) => `https://userservicebucket.s3.us-east-2.amazonaws.com/${Key}`)
-        resolve(allKeys);
-        return allKeys
-      })
-      .then(async S3Url => {
+    const ids = [];
+    // batchSize determines how many users get created simultaneously
+    // Use batchSize to not overload image service with too many requests at once
+    // Batches of 100+ requests known to cause socket error
+    let batchSize = 100;
 
-        const multipleUsers =  await Promise.all(ids.map(async id => {
-          const userData = {
-            userId: id,
-            name: faker.name.firstName(),
-            joinDate: faker.date.past(),
-            bio: faker.lorem.sentences(),
-            avatarUrl: S3Url[faker.random.number({ min: 0, max: 999 })],
-            isSuperhost: faker.random.boolean(),
-            identityVerified: faker.random.boolean(),
-            languages: faker.random.arrayElements(languages, faker.random.number({ min: 1, max: 3 })),
-            responseRate: faker.random.number({ min: 93, max: 100 }),
-            responseTime: 'within an hour'
-          };
-          return userData;
-        }))
+    while (number > 0 && batchSize > 0) {
+      ids.push(start);
+      start++;
+      number--;
+      batchSize--;
+    }
 
-        const response = await user.bulk({ docs: multipleUsers });
-        console.log('Batch complete!');
-        if(number > 0) {
-          seedManyUsers(start, number);
-        } else {
-          console.log('seeding complete')
-        }
-      })
-      .catch(err => {
-        console.log(err)
-        reject(err);
-      })
-  })
-};
+    const multipleUsers = await Promise.all(ids.map(async id => {
+      const userData = {
+        userId: id,
+        name: faker.name.firstName(),
+        joinDate: faker.date.past(),
+        bio: faker.lorem.sentences(),
+        avatarUrl: `https://userservicebucket.s3.us-east-2.amazonaws.com/${S3Url[faker.random.number({ min: 0, max: 999 })].Key}`,
+        isSuperhost: faker.random.boolean(),
+        identityVerified: faker.random.boolean(),
+        languages: faker.random.arrayElements(languages, faker.random.number({ min: 1, max: 3 })),
+        responseRate: faker.random.number({ min: 93, max: 100 }),
+        responseTime: 'within an hour'
+      };
+      return userData;
+    }))
 
-seedManyUsers(1000002, 1);
+    const response = await user1.bulk({ docs: multipleUsers });
+    console.log('Batch complete!');
+    if (number > 0) {
+      seedManyUsers(start, number);
+    } else {
+      console.log('seeding complete')
+    }
+  };
+  seedManyUsers(0, 10000000);
+}
+
+seed();
