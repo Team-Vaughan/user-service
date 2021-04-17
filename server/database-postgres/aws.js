2b06e4780b913ecf8c007a5cca3acc6ace5d9730
gigicupp <gigicupp@gmail.com>
2021-04-16 18:46:23 -0700

    sdk try

--- server/database-postgres/aws.js
+++ server/database-postgres/aws.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const AWS = require('aws-sdk');
 
 const S3 = new AWS.S3({
-  region: process.env.AWS_REGION,
+  region: 'us-east-2',
+  // region: process.env.AWS_REGION,
   apiVersion: 'latest',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  // aws-sdk: process.env.AWS_SDK_LOAD_CONFIG
 });
 
 let params = { Bucket: 'userservicebucket' };
